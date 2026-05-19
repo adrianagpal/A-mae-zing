3446b2aaaa86c0565de0bb3fea907814e97ff335
Adriana Gonzalez Palomo <adrianag@c3r4s2.42malaga.com>
2026-05-19 13:58:04 +0200

    generator flake8 and mypy fixed

--- mazegen_pkg/generator.py
+++ mazegen_pkg/generator.py
@@ -1,5 +1,8 @@
 import random
-from .high_definitions import MODIFIABLE, BARRIER, N, S, E, W, OPPOSITE, DIR_DELTA, DIRECTIONS
+from .high_definitions import MODIFIABLE, BARRIER
+from .high_definitions import S, E, OPPOSITE, DIR_DELTA, DIRECTIONS
+from typing import Any
+import numpy.typing as npt
 
 
 def in_bounds(grid: list[list[int]], r: int, c: int) -> bool:
@@ -10,45 +13,59 @@ def is_modifiable(grid: list[list[int]], r: int, c: int) -> bool:
     return grid[r][c] == MODIFIABLE
 
 
-#destroys a wall between cells, carving it
-def carve(grid: list[list[int]], r1: int, c1: int, r2: int, c2: int, direction: int,) -> bool:
+"""
+Destroys a wall between cells, carving it
+"""
+
+
+def carve(grid: list[list[int]],
+          r1: int, c1: int, r2: int, c2: int, direction: int,) -> bool:
+
     if grid[r1][c1] == BARRIER or grid[r2][c2] == BARRIER:
         return False
+
     grid[r1][c1] &= ~direction
     grid[r2][c2] &= ~OPPOSITE[direction]
     return True
 
 
-#set up for kruskal
+"""
+Set up for Kruskal's algorithm
+"""
+
+
 def collect_edges(grid: list[list[int]]) -> list[tuple[int, int, int]]:
     edges: list[tuple[int, int, int]] = []
-    r = 0
-    while r < len(grid):
-        c = 0
-        while c < len(grid[0]):
+
+    for r in range(len(grid)):
+
+        for c in range(len(grid[0])):
             if is_modifiable(grid, r, c):
                 if in_bounds(grid, r + 1, c) and is_modifiable(grid, r + 1, c):
                     edges.append((r, c, S))
                 if in_bounds(grid, r, c + 1) and is_modifiable(grid, r, c + 1):
                     edges.append((r, c, E))
-            c = c + 1
-        r = r + 1
+
     return edges
 
 
-#set up for prim
-def get_modifiable_neighbors(
-    grid: list[list[int]], r: int, c: int
-) -> list[tuple[int, int, int]]:
+"""
+Set up for Prim's algorithm
+"""
+
+
+def get_modifiable_neighbors(grid: list[list[int]],
+                             r: int, c: int) -> list[tuple[int, int, int]]:
+
     result: list[tuple[int, int, int]] = []
-    i = 0
-    while i < len(DIRECTIONS):
+
+    for i in range(len(DIRECTIONS)):
         d = DIRECTIONS[i]
         dr, dc = DIR_DELTA[d]
         nr, nc = r + dr, c + dc
         if in_bounds(grid, nr, nc) and is_modifiable(grid, nr, nc):
             result.append((d, nr, nc))
-        i = i + 1
+
     return result
 
 
@@ -56,13 +73,17 @@ def cell_index(grid: list[list[int]], r: int, c: int) -> int:
     return r * len(grid[0]) + c
 
 
-#union find, a list of 'structures' that will later join
+"""
+Union find, a list of 'structures' that will later join
+"""
+
+
 def make_uf(n: int) -> list[int]:
     uf: list[int] = []
-    i = 0
-    while i < n:
+
+    for i in range(n):
         uf.append(i)
-        i += 1
+
     return uf
 
 
@@ -81,35 +102,40 @@ def union(uf: list[int], x: int, y: int) -> bool:
     return True
 
 
-# Algo1: Kruskal's
+"""
+Kruskal's algorithm
+"""
+
+
 def kruskal(grid: list[list[int]]) -> list[list[int]]:
     uf = make_uf(len(grid) * len(grid[0]))
     edges = collect_edges(grid)
     random.shuffle(edges)
 
-    i = 0
-    while i < len(edges):
+    for i in range(len(edges)):
         r, c, d = edges[i]
         dr, dc = DIR_DELTA[d]
         nr, nc = r + dr, c + dc
         if union(uf, cell_index(grid, r, c), cell_index(grid, nr, nc)):
             carve(grid, r, c, nr, nc, d)
-        i += 1
 
     return grid
 
 
-# Algo2: Prim's
+"""
+Prim's algorithm
+"""
+
+
 def prim(grid: list[list[int]], start_r: int, start_c: int) -> list[list[int]]:
     frontier: list[tuple[int, int, int, int, int]] = []
 
     def push_frontiers(r: int, c: int) -> None:
         neighbors = get_modifiable_neighbors(grid, r, c)
-        i = 0
-        while i < len(neighbors):
+
+        for i in range(len(neighbors)):
             d, nr, nc = neighbors[i]
             frontier.append((r, c, d, nr, nc))
-            i += 1
 
     push_frontiers(start_r, start_c)
 
@@ -127,15 +153,16 @@ def prim(grid: list[list[int]], start_r: int, start_c: int) -> list[list[int]]:
     return grid
 
 
-def seal_isolated_pockets(grid: list[list[int]],entry: tuple[int, int],) -> None:
+def seal_isolated_pockets(grid: list[list[int]],
+                          entry: tuple[int, int]) -> None:
+
     reachable: set[tuple[int, int]] = {entry}
     queue: list[tuple[int, int]] = [entry]
-    qi = 0
-    while qi < len(queue):
+
+    for qi in range(len(queue)):
         r, c = queue[qi]
-        qi += 1
-        i = 0
-        while i < len(DIRECTIONS):
+
+        for i in range(len(DIRECTIONS)):
             d = DIRECTIONS[i]
             dr, dc = DIR_DELTA[d]
             nr, nc = r + dr, c + dc
@@ -146,24 +173,22 @@ def seal_isolated_pockets(grid: list[list[int]],entry: tuple[int, int],) -> None
             ):
                 reachable.add((nr, nc))
                 queue.append((nr, nc))
-            i += 1
 
-    r = 0
-    while r < len(grid):
-        c = 0
-        while c < len(grid[0]):
+    for r in range(len(grid)):
+
+        for c in range(len(grid[0])):
             if grid[r][c] != BARRIER and (r, c) not in reachable:
                 grid[r][c] = BARRIER
-            c += 1
-        r += 1
 
 
-def fill(mat, entry: tuple[int, int], algorithm: str, seed: int = 42,) -> list[list[int]]:
+def fill(mat: list[list[int]], entry: tuple[int, int],
+         algorithm: str, seed: int = 42) -> list[list[int]]:
+
     random.seed(seed)
     grid = mat
     seal_isolated_pockets(grid, entry)
 
-    if algorithm == 'kruskal': #this will aslo come from the config
+    if algorithm == 'kruskal':
         return kruskal(grid)
     elif algorithm == 'prim':
         return prim(grid, entry[0], entry[1])
